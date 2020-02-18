4fc62949953d3b7d1f1eef6893ef245a771260d9
djohnsonkc <djohnsonkc@gmail.com>
2020-02-18 12:55:57 -0600

    Initial commit

---
+++ React-Native/CalendarSync/CalendarSync.js
@@ -0,0 +1,356 @@
+import { Platform } from 'react-native'
+
+import RNCalendarEvents from 'react-native-calendar-events';
+
+import moment from 'moment'
+
+export var CalendarSync = {
+
+    // possible values: denied, restricted, authorized or undetermined
+    // STATUS_ENUMS: {
+    //     AUTHORIZED: 'authorized',
+    //     DENIED: 'denied',
+    //     RESTRICTED: 'restricted',
+    //     UNDETERMINED: 'undetermined'
+    // },
+
+    STATUS: null,
+    CALENDARS: [],
+    MYAPP_CALENDAR: null,
+    EVENTS: [],
+
+
+    PerformSync: function(action, schedule, statusEmitter, callback) {
+
+        //****************************************************
+        // Check if user has authorized access to their calendar
+        //****************************************************
+
+        // NOTE: statusEmitter is shared with other functions as they are called
+
+        CalendarSync.Authorize(statusEmitter, function(err, status){
+
+            if(err) {
+                callback(err, null)
+            }
+            else {
+
+                if(status === 'authorized') {
+
+                    CalendarSync.FindCalendars(statusEmitter, function(err, calendars){
+
+                        if(err) {
+                            callback(err, null)
+                        }
+                        else {
+
+                            if(statusEmitter) statusEmitter(null, {  message: "Calling CalendarSync.GetCalendarEvents..." })
+
+                            CalendarSync.GetCalendarEvents(calendars.calendars, statusEmitter, function(err, events){
+
+                                if(err) {
+                                    callback(err, null)
+                                }
+                                else {
+
+																	  // delete all from calendar before reinserting...
+                                    CalendarSync.DeleteMyAppEvents(events.myAppEvents, statusEmitter, function(err, resp){
+
+                                        if(err) {
+                                            callback(err, null)
+                                        }
+                                        else {
+																					
+																					// this allows the myApp calendar entries to be removed
+																					if(action === "delete-all") {
+																						callback(null, resp)
+																					}
+																					else {
+
+																						// re-insert shifts...
+                                            CalendarSync.AddMyAppEvents(calendars, schedule, statusEmitter, function(err, resp){
+
+                                                if(err) {
+                                                    callback(err, null)
+                                                }
+                                                else {
+                                                    callback(null, resp)
+                                                }
+
+                                            })
+																					}
+
+
+                                        }
+
+                                    })
+
+
+
+                                }
+
+
+                            })
+
+
+                        }
+                        
+
+                    })
+                }
+                else {
+                    callback( { error: 'Not authorized' }, null)
+                }
+
+            }
+
+        })
+
+
+
+    },
+
+    Authorize: function(statusEmitter, callback) {
+
+        if(statusEmitter) statusEmitter(null, { message: "RNCalendarEvents.authorizationStatus()...", status: '?' })
+
+
+        RNCalendarEvents.authorizationStatus()
+        .then((status) => {
+
+            // possible values: denied, restricted, authorized or undetermined
+
+            if(statusEmitter) statusEmitter(null, { message: "RNCalendarEvents.authorizationStatus", status: status })
+            
+            CalendarSync.STATUS = status
+        
+
+            // ONLY if undertermined, should we ask for permission
+            // user may later set to denied under device Settings->MyApp
+            // if(status !== 'undetermined') {
+
+                if(statusEmitter) statusEmitter(null, { message: "RNCalendarEvents.authorizeEventStore()..." })
+                
+                RNCalendarEvents.authorizeEventStore()
+                .then((status) => {
+
+                    CalendarSync.STATUS = status
+                    
+                    if(statusEmitter) statusEmitter(null, { message: "Did user authorize?", status: status })
+
+                    callback(null, status)
+
+                })
+                .catch(err => {
+                    if(statusEmitter) statusEmitter(err, null)
+                    callback(err, null)
+                });
+
+            // }
+            // else {
+            //     if(statusEmitter) statusEmitter(null, { message: "User has already authorized.", status: status })
+            //     callback(null, status)
+
+            // }
+
+            
+        })
+
+    },
+
+    CreateMyAppCalendar: function(statusEmitter, callback) {
+        
+        let request = {
+            title: 'MyApp',
+            color: '#31B0D5',
+            entityType: 'event',
+            name: 'MyApp',
+            accessLevel: 'owner',
+            ownerAccount: 'MyApp',
+            source: {
+                name: 'MyApp',
+                type: 'LOCAL',
+                isOwnerAccount: true
+            }
+        }
+
+        RNCalendarEvents.saveCalendar(request)
+        .then((calendar) => {
+
+            CalendarSync.MYAPP_CALENDAR = calendar
+            
+            if(statusEmitter) statusEmitter(null, { message: "MyApp Calendar Created", calendar: calendar })
+
+            callback(null, calendar)
+
+        })
+        .catch(err => {
+            if(statusEmitter) statusEmitter(err, null)
+            callback(err, null)
+        });
+
+    },
+
+    FindCalendars: function(statusEmitter, callback) {
+
+        if(statusEmitter) statusEmitter(null, { message: "FindCalendars..."})
+        
+
+        RNCalendarEvents.findCalendars()
+        .then((calendars) => {
+
+
+            if(statusEmitter) statusEmitter(null, {  message: "Found calendars", calendars: calendars })
+
+            CalendarSync.CALENDARS = calendars
+
+            let myAppCalendar = calendars.find(function(item){
+                return item.title === 'MyApp'
+            })
+
+            if(myAppCalendar) {
+                
+                if(statusEmitter) statusEmitter(null, { message: "Found MyApp Calendar", myAppCalendar: myAppCalendar })
+
+                CalendarSync.MYAPP_CALENDAR = myAppCalendar
+
+                callback(null, { calendars: calendars, myAppCalendar: myAppCalendar })
+            }
+            else {
+
+                if(statusEmitter) statusEmitter(null, { message: "Creating MyApp Calendar" })
+
+                CalendarSync.CreateMyAppCalendar(statusEmitter, function(err, newCalendar){
+
+                    if(err) {
+                        callback(err, null)
+                    }
+                    else {
+
+                        if(statusEmitter) statusEmitter(null, { message: "Created MyApp Calendar", myAppCalendar: newCalendar })
+
+                        CalendarSync.MYAPP_CALENDAR = newCalendar
+
+                        callback(null, { calendars: calendars, myAppCalendar: newCalendar })
+                    }
+                })
+            }
+            
+
+        })
+        .catch(err => {
+            if(statusEmitter) statusEmitter(err, null)
+            callback(err, null)
+        });
+
+
+    },
+
+
+    GetCalendarEvents: function(calendars, statusEmitter, callback) {
+
+        // next 30 days only - use this specific format
+        const startDate = moment(new Date()).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')  
+        const endDate = moment(new Date()).add(30,'days').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]') 
+
+        if(statusEmitter) statusEmitter(null, {  message: "GetCalendarEvents...", startDate: startDate, endDate, endDate, calendars: calendars })
+
+				let calendarIds = []
+				calendars.forEach(function(item){
+					calendarIds.push(item.id)
+				})
+
+        RNCalendarEvents.fetchAllEvents(startDate, endDate, calendarIds)
+        .then((events) => {
+
+						console.log("all events", events)
+            let myAppEvents  = events.filter(item => item.title.toLowerCase().indexOf('myApp:') !== -1);
+
+            CalendarSync.EVENTS = myAppEvents || []
+            
+            if(statusEmitter) statusEmitter(null, {  message: "All Events", events: events, myAppEvents: myAppEvents })
+
+            // use return here because .catch() seems to fire even when successful
+            callback(null, {  message: "All Events", events: events, myAppEvents: myAppEvents })
+
+        })
+        .catch(err => {
+            if(statusEmitter) statusEmitter({ message: "fetchAllEvents ERROR", error: err }, null)
+            return callback(err, null)
+        });
+
+
+
+    },
+
+    AddMyAppEvents: function(calendars, schedule, statusEmitter, callback) {
+
+        let isAndroid = () => Platform.OS.toUpperCase() === 'ANDROID'
+
+        let myAppCalendarId = calendars.myAppCalendar.id
+
+        schedule.forEach(function(s){
+
+            s.data.forEach(function(shift){
+
+							if(shift.type === 'scheduled-shift') {
+
+                let title = 'MyApp: ' + shift.role_Desc
+                if(shift.sect_Desc && shift.sect_Desc !== '') {
+                    title +=  ' @ ' + shift.sect_Desc
+                }
+
+                let settings = {
+                    calendarId: myAppCalendarId,
+                    startDate: moment(shift.shift_Begin).toISOString(),
+                    endDate: moment(shift.shift_End).toISOString(),
+                    location: shift.location_Name
+                }
+                // android providers "description" NOT "notes"
+                if(isAndroid) {
+                    settings.description = shift.event_desc
+                }
+                else {
+                    settings.notes = shift.event_desc
+                }
+
+								RNCalendarEvents.saveEvent(title, settings) 
+								
+							}
+
+            })
+
+        })
+
+        callback(null, { message: 'Added MyApp events to calendar.'})
+
+
+    },
+
+    DeleteMyAppEvents: function(myAppEvents, statusEmitter, callback) {
+
+        myAppEvents.forEach(function(event){
+            RNCalendarEvents.removeEvent(event.id)
+            .then((item) => {
+
+
+                if(statusEmitter) statusEmitter(null, {  message: "Deleted Event", event: item })
+
+
+            })
+            .catch(err => {
+                if(statusEmitter) statusEmitter({ message: "DeleteMyAppEvents ERROR", error: err }, null)
+                return callback(err, null)
+            })
+
+        })
+
+        callback(null, { message: "Deleted all MyApp events." })
+
+
+    }
+
+
+
+
+}
---
+++ React-Native/CalendarSync/README.md
@@ -0,0 +1,42 @@
+Calendar Sync Helper for React Native
+------------------
+
+This is an example of helper class for react-native-calendar-events to create custom calendar entries in a user's calendar. This solution isn't perfect,
+but I thought it might help others that trying to use react-native-calendar-events.
+
+In the example below, the 3rd parameter is a function that I use as a status "emitter" that I'm using to have some awareness of what is happening within the the nested asynchronous calls that are happening.
+
+CalendarSync.PerformSync handles all of the various things that need to occur when inserting custom 
+calendar entries. This implementation deletes all of the custom calendar entries 
+each time the list needs to be refreshed. Seems to perform well on various models and OS versions of iOS and Android devices.
+
+TODO: Use async waterfall to wrangle all of the async calls a bit better, making the code more readable and manageable, and avoid callback "hell".
+
+
+Code Sample
+------------------
+
+import { CalendarSync } from '../../Helpers/CalendarSync'
+
+CalendarSync.PerformSync("add", schedule, function(err, status) {
+
+    if(err) {
+        console.log("statusEmitter ERROR", JSON.stringify(err, null, 2))
+    }
+    else {
+        console.log("statusEmitter", JSON.stringify(status, null, 2))
+    }
+    
+
+}, function(err, finalResponse){
+
+    if(err) {
+        console.log("PerformSync ERROR", JSON.stringify(err, null, 2))
+    }
+    else {
+        console.log("PerformSync", JSON.stringify(finalResponse, null, 2))
+    }
+
+})
+
+
